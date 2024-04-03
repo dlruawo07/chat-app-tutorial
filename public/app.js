a0f467dea0748c7a944002f47b0b3b867a2817ec
kjlee <59638614+dlruawo07@users.noreply.github.com>
2024-04-04 03:10:45 +0900

    change address

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-const socket = io("ws://localhost:3001");
+const socket = io("https://chat-app-tutorial.onrender.com");
 
 const msgInput = document.querySelector("#message");
 const nameInput = document.querySelector("#name");
